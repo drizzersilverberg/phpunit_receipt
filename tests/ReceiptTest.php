dcdd067f200cf2c036712f1dc7bb1afeddf96b59
Dendi Handian <dendihandian@gmail.com>
2017-11-18 17:01:25 +0700

    add testTax() test

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -28,14 +28,37 @@ class ReceiptTest extends TestCase
         # -- output
         $output = $this->Receipt->total($input);
 
-        # -- define the sum of $numbers
+        # -- expected output
         $expected_output = 15;
 
-        # -- test receipt->total() function
+        # -- run test
         $this->assertEquals(
             $expected_output,
             $output,
             "When summing the total should equal $expected_output"
+        );
+    }
+
+    /** @test */
+    public function testTax()
+    {
+        # -- input
+        $inputAmount = 10.00;
+
+        # -- tax input
+        $taxInput = 0.10;
+
+        # -- output
+        $output = $this->Receipt->tax($inputAmount, $taxInput);
+
+        # -- expected output
+        $expected_output = 1.00;
+
+        # -- run test
+        $this->assertEquals(
+            $expected_output,
+            $output,
+            "The tax calculation should equal $expected_output"
         );
     }
 }
