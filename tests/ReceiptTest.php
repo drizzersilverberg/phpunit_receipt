553dc0d11847d53fad2c08436efad84bb9415790
Dendi Handian <dendihandian@gmail.com>
2017-11-18 16:50:08 +0700

    create setUp() and tearDown() for isolation test

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace TDD\Test;
 
@@ -7,24 +7,35 @@ require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_
 use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
-class ReceiptTest extends TestCase 
+class ReceiptTest extends TestCase
 {
+    public function setUp()
+    {
+        $this->Receipt = new Receipt;
+    }
+
+    public function tearDown()
+    {
+        unset($this->Receipt);
+    }
+
     /** @test */
     public function testTotal()
     {
-        $receipt = new Receipt();
+        # -- input
+        $input = [0,2,5,8];
 
-        # -- given array of number
-        $numbers = [0,2,5,8];
+        # -- output
+        $output = $this->Receipt->total($input);
 
         # -- define the sum of $numbers
-        $sum_of_numbers = 15;
+        $expected_output = 15;
 
         # -- test receipt->total() function
         $this->assertEquals(
-            $sum_of_numbers,
-            $receipt->total($numbers),
-            "When summing the total should equal $sum_of_numbers"
+            $expected_output,
+            $output,
+            "When summing the total should equal $expected_output"
         );
     }
 }
