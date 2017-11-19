e71b7f1012a1be5e0e7a3f2e85f54bd58ec052cc
Dendi Handian <dendihandian@gmail.com>
2017-11-19 20:28:46 +0700

    add testCurrenctAmt() test

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -123,4 +123,26 @@ class ReceiptTest extends TestCase
             "The tax calculation should equal $expected_output"
         );
     }
+
+    /**
+     * @dataProvider provideCurrencyAmt
+     */
+    public function testCurrencyAmt($input, $expected, $msg)
+    {
+        $this->assertSame(
+            $expected,
+            $this->Receipt->currencyAmt($input),
+            $msg
+        );
+    }
+
+    public function provideCurrencyAmt()
+    {
+        return [
+            [1, 1.00, '1 should be transformed into 1.00'],
+            [1.1, 1.10, '1.1 should be transformed into 1.10'],
+            [1.11, 1.11, '1.11 should stay as 1.11'],
+            [1.111, 1.11, '1.111 should be transformed into 1.11'],
+        ];
+    }
 }
